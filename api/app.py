738d8e68c0518c688b8c9dcb17017f336abcd9d7
Bhavya <ptbhavya97@gmail.com>
2026-09-17 18:39:02 +0530

    Prepare Flask API for deployment

--- api/app.py
+++ api/app.py
@@ -182,7 +182,8 @@ def predict():
 if __name__ == "__main__":
 
     app.run(
-        host="127.0.0.1",
+        #host="127.0.0.1",
+        host="0.0.0.0",
         port=5000,
         debug=True
     )
